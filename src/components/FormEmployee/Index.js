e3531fff660d5b2d21ce7bc3ee2a9e21190cc6e7
OnlyManullang <66237057+wanrimanullang@users.noreply.github.com>
2023-06-11 17:31:58 +0700

    can input data for employee

--- src/components/FormEmployee/Index.js
+++ src/components/FormEmployee/Index.js
@@ -1,5 +1,5 @@
-import React, {useState} from "react";
-
+import React, { useState } from "react";
+import axios from "axios";
 import {
   Button,
   Modal,
@@ -10,21 +10,57 @@ import {
   Input,
   Label,
   Form,
+  Alert,
 } from "reactstrap";
 
 function FormEmployee() {
   const [modal, setModal] = useState(false);
-  const [nestedModal, setNestedModal] = useState(false);
-  const [closeAll, setCloseAll] = useState(false);
+  const [email, setEmail] = useState("");
+  const [name, setName] = useState("");
+  const [password, setPassword] = useState("");
+  const [unit, setUnit] = useState("");
+  const [position, setPosition] = useState("");
+  const [successMessage, setSuccessMessage] = useState("");
+  const [errorMessage, setErrorMessage] = useState("");
 
   const toggle = () => setModal(!modal);
-  const toggleNested = () => {
-    setNestedModal(!nestedModal);
-    setCloseAll(false);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      const token = localStorage.getItem("token");
+      const response = await axios.post(
+        "http://localhost:8000/api/users",
+        {
+          name,
+          email,
+          password,
+          // unit,
+          // job_position: position,
+        },
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        }
+      );
+      console.log(response);
+      setSuccessMessage("User created successfully");
+      setName("");
+      setEmail("");
+      setPassword("");
+      // setUnit("");
+      // setPosition("");
+    } catch (error) {
+      setErrorMessage("Error creating user");
+      console.error("Error creating user:", error);
+    }
   };
-  const toggleAll = () => {
-    setNestedModal(!nestedModal);
-    setCloseAll(true);
+
+  const handleDismiss = () => {
+    setSuccessMessage("");
+    setErrorMessage("");
   };
 
   return (
@@ -36,7 +72,28 @@ function FormEmployee() {
         <Modal isOpen={modal} toggle={toggle}>
           <ModalHeader toggle={toggle}>Form Employee</ModalHeader>
           <ModalBody>
-            <Form>
+            {successMessage && (
+              <Alert color="success" toggle={handleDismiss}>
+                {successMessage}
+              </Alert>
+            )}
+            {errorMessage && (
+              <Alert color="danger" toggle={handleDismiss}>
+                {errorMessage}
+              </Alert>
+            )}
+            <Form onSubmit={handleSubmit}>
+              <FormGroup>
+                <Label for="exampleName">Name</Label>
+                <Input
+                  id="exampleName"
+                  name="name"
+                  placeholder="Name"
+                  type="name"
+                  value={name}
+                  onChange={(e) => setName(e.target.value)}
+                />
+              </FormGroup>
               <FormGroup>
                 <Label for="exampleEmail">Email</Label>
                 <Input
@@ -44,6 +101,8 @@ function FormEmployee() {
                   name="email"
                   placeholder="Email"
                   type="email"
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
                 />
               </FormGroup>
               <FormGroup>
@@ -53,6 +112,8 @@ function FormEmployee() {
                   name="password"
                   placeholder="password"
                   type="password"
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
                 />
               </FormGroup>
               <FormGroup>
@@ -62,40 +123,35 @@ function FormEmployee() {
                   name="unit"
                   placeholder="Unit"
                   type="unit"
+                  value={unit}
+                  onChange={(e) => setUnit(e.target.value)}
                 />
               </FormGroup>
               <FormGroup>
                 <Label for="examplePosition">Position</Label>
-                <Input id="examplePosition" name="position" type="select">
+                <Input
+                  id="examplePosition"
+                  name="position"
+                  type="select"
+                  value={position}
+                  onChange={(e) => setPosition(e.target.value)}
+                >
                   <option>Software Engineer</option>
                   <option>Doctor</option>
                   <option>Nurse</option>
-                  <option>Accountan</option>
+                  <option>Accountant</option>
                   <option>Manager</option>
                 </Input>
               </FormGroup>
+              <Button color="success" type="submit">
+                Done
+              </Button>
             </Form>
           </ModalBody>
           <ModalFooter>
-            <Button color="success" onClick={toggleNested}>
-              Done
+            <Button color="secondary" onClick={toggle}>
+              Cancel
             </Button>
-            <Modal
-              isOpen={nestedModal}
-              toggle={toggleNested}
-              onClosed={closeAll ? toggle : undefined}
-            >
-              <ModalHeader>Check again</ModalHeader>
-              <ModalBody></ModalBody>
-              <ModalFooter>
-                <Button color="primary" onClick={toggleNested}>
-                  Done
-                </Button>{" "}
-                <Button color="secondary" onClick={toggleAll}>
-                  Cancel
-                </Button>
-              </ModalFooter>
-            </Modal>
           </ModalFooter>
         </Modal>
       </div>
