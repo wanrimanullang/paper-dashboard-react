887a349341ffca17e799b4bcab98d8e342c678ce
OnlyManullang <66237057+wanrimanullang@users.noreply.github.com>
2023-06-11 12:00:21 +0700

    will login if you need for see dashboard

--- src/views/Dashboard.js
+++ src/views/Dashboard.js
@@ -1,49 +1,63 @@
 import React from "react";
+import { Card, Row, Col } from "reactstrap";
 import FormEmployee from "components/FormEmployee/Index";
 import FormPosition from "components/FormPosition/Index";
 import FormUnit from "components/FormUnit/Index";
 import InformationEmployees from "components/InformationEmployees/Index";
-
 import TopEmployeesTable from "components/TopEmployeeTable";
 import PositionTable from "components/PositionTable/Index";
 import UnitTable from "components/UnitTable/Index";
 import EmployeesTable from "components/EmployeesTable";
 
-import { Card, Row, Col } from "reactstrap";
-
 function Dashboard() {
+  const isLoggedIn = !!localStorage.getItem("auth");
+
+  if (!isLoggedIn) {
+    return (
+      <div className="content">
+        <Row>
+          <Col md="12">
+            <Card>
+              <h4 className="text-center p-3">Please login to access the dashboard.</h4>
+            </Card>
+          </Col>
+        </Row>
+      </div>
+    );
+  }
+
   return (
     <>
       <div className="content">
-        <InformationEmployees></InformationEmployees>
+        <InformationEmployees />
         <Row>
           <Col md="12">
             <Card>
-              <TopEmployeesTable></TopEmployeesTable>
+              <TopEmployeesTable />
             </Card>
           </Col>
         </Row>
         <Row>
           <Col md="12">
-            <FormEmployee></FormEmployee>
+            <FormEmployee />
             <Card>
-              <EmployeesTable responsive></EmployeesTable>
+              <EmployeesTable responsive />
             </Card>
           </Col>
         </Row>
         <Row>
           <Col md="12">
-            <FormUnit></FormUnit>
+            <FormUnit />
             <Card>
-              <UnitTable></UnitTable>
+              <UnitTable />
             </Card>
           </Col>
         </Row>
         <Row>
           <Col md="12">
-            <FormPosition></FormPosition>
+            <FormPosition />
             <Card>
-              <PositionTable></PositionTable>
+              <PositionTable />
             </Card>
           </Col>
         </Row>
